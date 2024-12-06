fb87e6ac9fbc81da4322a16af3b2f72d6b467607
Kimaya D Sawant <kdsawant@csuchico.edu>
2024-12-05 18:04:27 -0800

    commiting all files

---
+++ lib/src/state/auth/backend/authenticator.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:instagramz_clone/src/state/auth/constants/constants.dart';
+import 'package:instagramz_clone/src/state/auth/models/auth_result.dart';
+import 'package:instagramz_clone/src/state/posts/typedefs/user_id.dart';
+
+class Authenticator {
+  const Authenticator();
+
+  // Getters
+
+  bool get isAlreadyLoggedIn => userId != null;
+  UserId? get userId => FirebaseAuth.instance.currentUser?.uid;
+  String get displayName =>
+      FirebaseAuth.instance.currentUser?.displayName ?? '';
+  String? get email => FirebaseAuth.instance.currentUser?.email;
+
+  Future<void> logOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+    // Removed Facebook logout
+  }
+
+  // Removed Facebook login method
+
+  Future<AuthResult?> loginWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        Constants.emailScope,
+      ],
+    );
+    final signInAccount = await googleSignIn.signIn();
+    if (signInAccount == null) {
+      return AuthResult.aborted;
+    }
+
+    final googleAuth = await signInAccount.authentication;
+    final oauthCredentials = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+      accessToken: googleAuth.accessToken,
+    );
+    try {
+      await FirebaseAuth.instance.signInWithCredential(
+        oauthCredentials,
+      );
+      return AuthResult.success;
+    } catch (e) {
+      return AuthResult.failure;
+    }
+  }
+}
